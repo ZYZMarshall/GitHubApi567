cc596f5cf35268a10355c955d0f8797f1bfe035e
ZYZMarshall <42755788+ZYZMarshall@users.noreply.github.com>
2018-09-23 12:00:00 -0400

    Add files via upload

---
+++ HW04567.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+"""
+You should write a function that will take as input a GitHub user ID. 
+The output from the function will be a list of the names of the repositories that the user has, 
+along with the number of commits that are in each of the listed repositories.
+
+"""
+def getUserRepos(userName):       
+    API = ("https://api.github.com/users/" + userName + "/repos")
+    userData = requests.get(API)
+    repositories = json.loads(userData.text)
+    userRepos = []
+    
+    for repository in repositories:
+        try:
+            userRepos.append(repository.get("name"))
+        except:
+            userRepos = []
+         
+    return userRepos
+
+def getCommitnum(userName, repoName):          #  number of commits that are in each of the listed repositories.
+    API = "https://api.github.com/repos/" + userName + "/" + repoName + "/commits"
+    repoData = requests.get(API)
+    commits = json.loads(repoData.text)
+    Commitnum = len(commits)
+
+    return Commitnum
+        
+"""
+Main function that lists all the repos and lists each commit count given a specific
+github user name.
+"""
+if __name__ == "__main__":
+    userName = input("Enter Github username: ")     #Ask for username
+    userRepos = getUserRepos(userName) # Get Respos according to the username
+    print("User: " + userName)
+    for repository in userRepos:              #Use for Loop to printing name and their commit in associated repos 
+        Commitnum = getCommitnum(userName, repository)
+        print("Repo: " + repository + " Number of Commits: " + str(Commitnum))
---
+++ testHW04567.py
@@ -0,0 +1,29 @@
+import unittest
+from HW04567 import getUserRepos, getCommitnum
+
+class TestHW04567(unittest.TestCase):
+    
+    def testHW0456701(self):
+        self.assertIn("hello-world", getUserRepos("ZYZMarshall"))
+                    
+    def testHW0456701b(self):
+        self.assertNotIn("ddddddd", getUserRepos("ZYZMarshall"))
+
+    def testHW0456701c(self):
+        self.assertEqual(24, getCommitnum("ZYZMarshall", "Triangle567"))
+                         
+    def testHW0456702A(self):
+        self.assertIn("Triangle567", getUserRepos("ZYZMarshall"))
+                    
+    def testHW0456702b(self):
+        self.assertNotIn("HelloUnivrse", getUserRepos("ZYZMarshall"))
+
+    def testHW0456702c(self):
+        self.assertNotEqual(5, getCommitnum("ZYZMarshall", "Hello-world-"))
+
+    def testHW0456703A(self):
+        self.assertEqual([], getUserRepos("Notexisit12343213"))
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
