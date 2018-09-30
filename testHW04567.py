75d84e08eec57cb3d42f97799896a412dd3478ee
ZYZMarshall <42755788+ZYZMarshall@users.noreply.github.com>
2018-09-30 18:27:09 -0400

    Update testHW04567.py

--- testHW04567.py
+++ testHW04567.py
@@ -5,10 +5,10 @@ from HW04567 import  getCommits, getRepos
 class TestGitHubApi(unittest.TestCase):
 
     def testGetCommits(self):
-        self.assertEqual(getCommits('ZYZMarshall', ['Triangle567']), [24], 'There are 24 commits in this repo')
+        self.assertEqual(getCommits('ZYZMarshall', ['Triangle567']), [2], 'There are 2 commits in this repo')
 
     def testGetRepos(self):
-        self.assertIn(getRepos('ZYZMarshall'), ['GitHubApi567', 'Hello-world-', 'Triangle567'], 'GitHubApi567, Hello-world-, and Triangle567 are in the repos')
+        self.assertEqual(getRepos('ZYZMarshall'), ['hello-world', 'triangle', 'SSW567HW05','GitHubApi567', 'Hello-world-', 'Triangle567'])
 
 if __name__ == '__main__':
     unittest.main()
