4d9acfeccd90369435fb069579b9da6b25d4955b
ZYZMarshall <42755788+ZYZMarshall@users.noreply.github.com>
2018-09-30 19:36:22 -0400

    Update HW04567.py

--- HW04567.py
+++ HW04567.py
@@ -1,40 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
-HW04-API
+Created on Tue Feb 13 18:03:11 2018
+@author: allo0o2a
 """
-import requests
+
 import json
+import requests
 
 
-def get_user_repositories(user_id):
-    url = 'https://api.github.com/users/' + user_id + '/repos'
-    response = json.loads(requests.get(url).text)
-    rt = []
-    for i in response:
-        rt.append(i.get('name'))
-    return rt
+def get_info(user_id):
+    api_url1 = requests.get('https://api.github.com/users/' + user_id + '/repos')
+    response_json1 = json.loads(api_url1.text)
+    
+    try:
+        response_json1[0]['name']
+    except: 
+        return ('Invalid GitHub Username')
+    
+    li = []
+    for item in response_json1:  
+        api_url_2 = requests.get('https://api.github.com/repos/' + user_id + '/' + item['name'] + '/commits')
+        response_json2 = json.loads(api_url_2.text)
+        
+        count = 0
+        #Empty Repo is dict not list.
+        if isinstance (response_json2,list):
+            for i in response_json2:
+                try:
+                    i['commit']
+                    count+=1
+                except KeyError: 
+                    pass
+        li.append((item['name'],count))
+    return li
 
+def main():
+    user_id = input("Please enter your GitHub Username: ")
+    print(get_info(user_id))
 
-def get_commits(user_id, repository):
-    url = 'https://api.github.com/repos/' + user_id + '/' + repository + '/commits'
-    response = json.loads(requests.get(url).text)
-    return len(response)
-
-
-def get_commits_numbers(user_id):
-    repositories = get_user_repositories(user_id)
-    commits_number = {}
-    for repository in repositories:
-        commits_number[repository] = get_commits(user_id, repository)
-    return commits_number
-
-
-def print_info(user_id):
-    dic = get_commits_numbers(user_id)
-    print('User: ' + user_id)
-    for repository in dic:
-        print('Repo: ' + repository + " Number of commits: " + str(dic[repository]))
-
-
-if __name__ == '__main__':
-    user_id = "ZYZMarshall"
-    print_info(user_id)
+      
+if __name__ == "__main__":
+    main()
