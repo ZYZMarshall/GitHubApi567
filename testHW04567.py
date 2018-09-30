3fe4216aa218a600c81e8335e034d3163eaef771
ZYZMarshall <42755788+ZYZMarshall@users.noreply.github.com>
2018-09-30 18:55:24 -0400

    Update testHW04567.py

--- testHW04567.py
+++ testHW04567.py
@@ -6,4 +6,4 @@ from HW04567 import get_commits_numbers
 class TestHW04(unittest.TestCase):
     def test_user_repositories(self):
         self.assertDictEqual(get_commits_numbers("ZYZMarshall"),
-                             {'GitHubAPI567': 18, 'Triangle567': 24, 'Hello-world-': 5})
+                             {{'GitHubApi567': 20,'GitHubApi567-1': 5,'GitHubApi567-2': 4,'Hello-world-': 5, 'SSW567-HW05': 5,'Triangle567': 24,'hello-world': 2, 'triangle': 5}})
