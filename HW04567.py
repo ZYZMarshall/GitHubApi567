6d402e2d5ea6ed2bc8a361d665c9b889f7c158d1
ZYZMarshall <42755788+ZYZMarshall@users.noreply.github.com>
2018-09-30 17:56:54 -0400

    Update HW04567.py

--- HW04567.py
+++ HW04567.py
@@ -1,42 +1,36 @@
 import requests
 import json
 
-"""
-You should write a function that will take as input a GitHub user ID. 
-The output from the function will be a list of the names of the repositories that the user has, 
-along with the number of commits that are in each of the listed repositories.
+def getUserID():
+    userID = input('Enter a GitHub User ID: ')
+    return userID
 
-"""
-def getUserRepos(userName):       
-    API = ("https://api.github.com/users/" + userName + "/repos")
-    userData = requests.get(API)
-    repositories = json.loads(userData.text)
-    userRepos = []
-    
-    for repository in repositories:
-        try:
-            userRepos.append(repository.get("name"))
-        except:
-            userRepos = []
-         
-    return userRepos
+def getCommits(userID, repos):
+    commits = []
+    for i in repos:
+        r = requests.get('https://api.github.com/repos/' + userID + '/' + i + '/commits')
+        r = r.json()
+        commits.append(len(r))
+    return commits
 
-def getCommitnum(userName, repoName):          #  number of commits that are in each of the listed repositories.
-    API = "https://api.github.com/repos/" + userName + "/" + repoName + "/commits"
-    repoData = requests.get(API)
-    commits = json.loads(repoData.text)
-    Commitnum = len(commits)
 
-    return Commitnum
-        
-"""
-Main function that lists all the repos and lists each commit count given a specific
-github user name.
-"""
-if __name__ == "__main__":
-    userName = input("Enter Github username: ")     #Ask for username
-    userRepos = getUserRepos(userName) # Get Respos according to the username
-    print("User: " + userName)
-    for repository in userRepos:              #Use for Loop to printing name and their commit in associated repos 
-        Commitnum = getCommitnum(userName, repository)
-        print("Repo: " + repository + " Number of Commits: " + str(Commitnum))
+def getRepos(userID):
+    r = requests.get('https://api.github.com/users/' + userID + '/repos')
+    r = r.json()
+    repos = []
+    for i in r:
+        #print(i['name'])
+        repos.append(i['name'])
+    return repos
+
+
+if __name__ == '__main__':
+    userID = getUserID()
+    repos = getRepos(userID)
+    commits = getCommits(userID, repos)
+
+    print('User: ' + userID)
+    j = 0
+    for i in repos:
+        print('Repo: ' + i + ' Number of Commits: ' + str(commits[j]))
+        j += 1
