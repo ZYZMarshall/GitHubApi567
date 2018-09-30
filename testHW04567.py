3ac0a47be78b8875427d8d75f12a72612f63fbd2
ZYZMarshall <42755788+ZYZMarshall@users.noreply.github.com>
2018-09-30 18:02:07 -0400

    Update testHW04567.py

--- testHW04567.py
+++ testHW04567.py
@@ -1,29 +1,14 @@
 import unittest
-from HW04567 import getUserRepos, getCommitnum
 
-class TestHW04567(unittest.TestCase):
-    
-    def testHW0456701(self):
-        self.assertIn("hello-world", getUserRepos("ZYZMarshall"))
-                    
-    def testHW0456701b(self):
-        self.assertNotIn("ddddddd", getUserRepos("ZYZMarshall"))
+from GitHubApi import getCommits, getRepos
 
-    def testHW0456701c(self):
-        self.assertEqual(24, getCommitnum("ZYZMarshall", "Triangle567"))
-                         
-    def testHW0456702A(self):
-        self.assertIn("Triangle567", getUserRepos("ZYZMarshall"))
-                    
-    def testHW0456702b(self):
-        self.assertNotIn("HelloUnivrse", getUserRepos("ZYZMarshall"))
+class TestGitHubApi(unittest.TestCase):
 
-    def testHW0456702c(self):
-        self.assertNotEqual(3, getCommitnum("ZYZMarshall", "Hello-world-"))
+    def testGetCommits(self):
+        self.assertEqual(getCommits('ZYZMarshall', ['Triangle567']), [24], 'There are 24 commits in this repo')
 
-    def testHW0456703A(self):
-        self.assertEqual([], getUserRepos("Notexisit12343213"))
+    def testGetRepos(self):
+        self.assertEqual(getRepos('ZYZMarshall'), ['GitHubApi567', 'Hello-world-', 'Triangle567'], 'GitHubApi567, Hello-world-, and Triangle567 are the repos')
 
 if __name__ == '__main__':
-    print('Running unit tests')
     unittest.main()
