ab5e44680a7d30d1a45b6f7f8bf8c72952fb7c58
ZYZMarshall <42755788+ZYZMarshall@users.noreply.github.com>
2018-09-30 22:02:22 -0400

    Update testHW04567.py

--- testHW04567.py
+++ testHW04567.py
@@ -1,24 +1,29 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tuesday 9/25 2018
-author: yuzhen zhang
-"""
-
 import unittest
+from HW04567 import getUserRepos, getCommitnum
 
-from HW04567 import get_info
-
-
-class TestGetInfo(unittest.TestCase):
+class TestHW04567(unittest.TestCase):
     
-    def testValidUser(self):
-        self.assertEqual(get_info('ZYZMarshall'), [('hello-world', 2), ('GitHubApi567', 25), ('Triangle567', 24)])
-        
-    def testInvalidUser(self):
-        self.assertEqual(get_info('jsbfjejknejk'), 'Invalid GitHub Username')
-        self.assertEqual(get_info(' '), 'Invalid GitHub Username')
+    def testHW0456701(self):
+        self.assertIn("hello-world", getUserRepos("ZYZMarshall"))
+                    
+    def testHW0456701b(self):
+        self.assertNotIn("ddddddd", getUserRepos("ZYZMarshall"))
 
+    def testHW0456701c(self):
+        self.assertEqual(24, getCommitnum("ZYZMarshall", "Triangle567"))
+                         
+    def testHW0456702A(self):
+        self.assertIn("Triangle567", getUserRepos("ZYZMarshall"))
+                    
+    def testHW0456702b(self):
+        self.assertNotIn("HelloUnivrse", getUserRepos("ZYZMarshall"))
+
+    def testHW0456702c(self):
+        self.assertNotEqual(3, getCommitnum("ZYZMarshall", "Hello-world-"))
+
+    def testHW0456703A(self):
+        self.assertEqual([], getUserRepos("Notexisit12343213"))
 
 if __name__ == '__main__':
+    print('Running unit tests')
     unittest.main()
